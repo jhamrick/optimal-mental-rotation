0fed19119b96b6ee34eb96c271c397d1c37b38ee
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-04-28 19:52:49 -0700

    More efficient image rendering

--- code/util.py
+++ code/util.py
@@ -1,13 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import scipy.ndimage as nd
-import tempfile
+import PIL
 
-from joblib import Memory
-memory = Memory(cachedir='cache', verbose=0)
-
-from snippets.graphing import save
+from snippets.graphing import plot_to_array
 
 
 def make_stimulus(npoints, rso):
@@ -20,6 +16,77 @@ def make_stimulus(npoints, rso):
     return X
 
 
+def plot_stimulus(X):
+    plt.plot(X[:, 0], X[:, 1],
+             color='k',
+             linewidth=2)
+    plt.xticks([], [])
+    plt.yticks([], [])
+    plt.axis([-0.5, 0.5, -0.5, 0.5])
+    plt.box('off')
+
+
+def plot_stimuli(**kwargs):
+    """Plot the original stimulus and it's rotated counterpart.
+
+    """
+    nstim = len(kwargs)
+
+    plt.clf()
+    fig = plt.gcf()
+    fig.set_figwidth(nstim*2.25)
+    fig.set_figheight(2)
+
+    Xs = sorted(kwargs.keys())
+    for i, X in enumerate(Xs):
+        plt.subplot(1, nstim, i+1)
+        plot_stimulus(kwargs[X])
+        plt.title("$%s$" % X)
+
+
+def plot_images(**kwargs):
+    nimg = len(kwargs)
+
+    plt.clf()
+    fig = plt.gcf()
+    fig.set_figwidth(nimg*2.25)
+    fig.set_figheight(2)
+
+    Is = sorted(kwargs.keys())
+    for i, I in enumerate(Is):
+        plt.subplot(1, nimg, i+1)
+        plt.imshow(kwargs[I], cmap='gray', vmin=0, vmax=1)
+        plt.xticks([], [])
+        plt.yticks([], [])
+        plt.box('off')
+        plt.title("$%s$" % I)
+
+
+def render(X):
+    """Render the shape into pixel-space."""
+
+    # plot the stimulus
+    fig = plt.figure()
+    fig.clf()
+    fig.set_figwidth(2)
+    fig.set_figheight(2)
+    # turn off the gray frame
+    fig.frameon = False
+    # make the plot fit the full figure width, since we don't have
+    # axis/tick labels or a title, etc.
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    plot_stimulus(X)
+
+    # convert the image to pixels and close it
+    img = plot_to_array(fig)
+    plt.close(fig)
+
+    # convert it to grayscale to save space
+    grayscale = np.sqrt(np.sum(img**2, axis=2) / 3.)
+
+    return grayscale
+
+
 def make_image(X, sigma, rso):
     """Jitter the vertices of the shape using Gaussian noise with variance
     `sigma`.
@@ -29,42 +96,8 @@ def make_image(X, sigma, rso):
         I = X.copy()
     else:
         I = X + rso.normal(0, sigma, X.shape)
-    return I
-
-
-@memory.cache
-def render(I):
-    """Render the shape into pixel-space.
-
-    Note: this function is memoized. It is also horribly inefficient.
-
-    """
-    # create a temporary file to render the shape to
-    fh = tempfile.NamedTemporaryFile()
-    filename = fh.name + '.png'
-    fh.close()
-
-    # plot the shape
-    fig = plt.figure()
-    fig.clf()
-    fig.set_figwidth(2)
-    fig.set_figheight(2)
-    plt.plot(I[:, 0], I[:, 1],
-             color='k',
-             linewidth=2)
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.axis([-0.5, 0.5, -0.5, 0.5])
-    plt.box('off')
-    # save it to the temporary file name
-    save(filename, width=4, height=4)
-
-    # load it back in as pixels
-    img = mpimg.imread(filename)
-    # convert it to grayscale to save space
-    grayscale = np.sqrt(np.sum(img[:, :, :-1]**2, axis=2) / 3.)
-
-    return grayscale
+    rI = render(I)
+    return rI
 
 
 def blur(img, blur):
@@ -94,42 +127,8 @@ def rotate(Xa, theta):
     return Xb
 
 
-def plot_stimuli(Xa, Ia, Xb, Ib):
-    """Plot the original stimulus and it's rotated counterpart, as well as
-    noisy versions of each.
-
-    """
-    plt.clf()
-    fig = plt.gcf()
-    fig.set_figwidth(9)
-    fig.set_figheight(4)
-
-    plt.subplot(1, 2, 1)
-    plt.plot(Xa[:, 0], Xa[:, 1],
-             color='k',
-             linewidth=2,
-             label="$X_a$")
-    plt.plot(Ia[:, 0], Ia[:, 1],
-             color='k',
-             alpha=0.5,
-             linewidth=2,
-             label="$I_a$")
-    plt.axis([-0.5, 0.5, -0.5, 0.5])
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.legend(loc=4, numpoints=1)
-
-    plt.subplot(1, 2, 2)
-    plt.plot(Xb[:, 0], Xb[:, 1],
-             color='b',
-             linewidth=2,
-             label="$X_b$")
-    plt.plot(Ib[:, 0], Ib[:, 1],
-             color='b',
-             alpha=0.5,
-             linewidth=2,
-             label="$I_b$")
-    plt.axis([-0.5, 0.5, -0.5, 0.5])
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.legend(loc=4, numpoints=1)
+def rotate_image(I, theta):
+    img = PIL.Image.fromarray(1-I)
+    rimg = img.rotate(np.degrees(theta))
+    rI = 1-np.array(rimg)
+    return rI
