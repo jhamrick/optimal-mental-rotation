12ec86ccdb3905f96b9a396e0d3bafaaa5153676
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-06 13:58:27 -0700

    Flesh out docstrings in model.py

--- code/model.py
+++ code/model.py
@@ -12,42 +12,91 @@ from snippets.stats import GP
 from snippets.stats import periodic_kernel as periodic_kernel
 
 
-def similarity(I0, I1, sf=1):
-    """Computes the similarity between images `I0` and `I1`."""
-    diff = exp(np.sum(-0.5 * (I0 - I1)**2 / (sf**2))) + 1
-    return diff
+class PeriodicMLL(object):
+    """Object representing the marginal log likelihood (MLL) of a periodic
+    kernel function.
 
+    Methods
+    -------
+    __call__
+        Compute the MLL.
+    jacobian
+        Compute the Jacobian of the MLL.
+    maximize
+        Find kernel parameter values which maximize the MLL.
 
-class GP_MarginalLogLikelihood(object):
+    """
 
     def __init__(self):
+        # create symbolic variables
         d = sym.Symbol('d')
         h = sym.Symbol('h')
         w = sym.Symbol('w')
         s = sym.Symbol('s')
 
+        # symbolic version of the periodic kernel function
         k1 = (h ** 2) * sym.exp(-2. * sym.sin(d / 2.) ** 2 / (w ** 2))
         k2 = (s ** 2) * delta(d)
         self.sym_K = k1 + k2
 
+        # compute partial derivatives
         self.sym_dK_dh = sym.diff(self.sym_K, h)
-        self.dK_dh = lambdify(((h, w, s), d), self.sym_dK_dh)
-
         self.sym_dK_dw = sym.diff(self.sym_K, w)
-        self.dK_dw = lambdify(((h, w, s), d), self.sym_dK_dw)
-
         self.sym_dK_ds = sym.diff(self.sym_K, s)
+
+        # turn partial derivatives into functions, so we can evaluate
+        # them (to compute the Jacobian)
+        self.dK_dh = lambdify(((h, w, s), d), self.sym_dK_dh)
+        self.dK_dw = lambdify(((h, w, s), d), self.sym_dK_dw)
         self.dK_ds = lambdify(((h, w, s), d), self.sym_dK_ds)
 
     @staticmethod
     def _d_dthetaj(Ki, dK_dthetaj, theta, x, y):
+        """Compute the partial derivative of the marginal log likelihood with
+        respect to one of its parameters, `\theta_j`.
+
+        This is computing Eq. 5.9 of Rasmussen & Williams (2006):
+
+        $$\frac{\partial}{\partial\theta_j}\log{p(\mathbf{y} | X, \mathbf{\theta})}=\frac{1}{2}\mathbf{y}^\top K^{-1}\frac{\partial K}{\partial\theta_j}K^{-1}\mathbf{y}-frac{1}{2}\mathrm{tr}(K^{-1}\frac{\partial K}{\partial\theta_j})$$
+
+        Parameters
+        ----------
+        Ki : numpy.ndarray with shape (n, n)
+            Inverse of the kernel matrix `K`
+        dK_dthetaj : function((h, w, s), d)
+            Computes the partial derivative of `K` with respect to
+            `theta_j`, where `theta_j` is `h`, `w`, or `s`. Takes as
+            arguments the kernel parameters and the difference
+            `x_1-x_2`.
+        theta : 3-tuple
+            The kernel parameters `h`, `w`, and `s`.
+        x : numpy.ndarray with shape (n,)
+            Given input values
+        y : numpy.ndarray with shape (n,)
+            Given output values
+
+        Returns
+        -------
+        out : float
+            Partial derivative of the marginal log likelihood.
+
+        References
+        ----------
+        Rasmussen, C. E., & Williams, C. K. I. (2006). Gaussian processes
+            for machine learning. MIT Press.
+
+        """
+
         h, w, s = theta
+
+        # compute dK/dtheta_j matrix
         dj = np.empty((x.size, x.size))
         for i in xrange(x.size):
             for j in xrange(x.size):
                 diff = x[i] - x[j]
                 dj[i, j] = dK_dthetaj((h, w, s), diff)
 
+        # compute the partial derivative of the marginal log likelihood
         k = dot(Ki, dj)
         t0 = 0.5 * dot(y.T, dot(k, dot(Ki, y)))
         t1 = -0.5 * trace(k)
@@ -56,7 +105,26 @@ class GP_MarginalLogLikelihood(object):
         return dd
 
     def __call__(self, theta, x, y):
-        """
+        """Computes the marginal negative log likelihood of the kernel.
+
+        This is computing Eq. 5.8 of Rasmussen & Williams (2006):
+
+        $$\log{p(\mathbf{y} | X,\mathbf{\theta})} = -\frac{1}{2}\mathbf{y}^\top K_y^{-1}\mathbf{y} - \frac{1}{2}\log{|K_y|} - \frac{n}{2}\log{2\pi}
+
+        Parameters
+        ----------
+        theta : 3-tuple
+            The kernel parameters `h`, `w`, and `s`
+        x : numpy.ndarray with shape (n,)
+            The given input values
+        y : numpy.ndarray with shape (n,)
+            The given output values
+
+        Returns
+        -------
+        out : float
+            The marginal negative log likelihood of the data given the
+            parameters
 
         References
         ----------
@@ -73,15 +141,15 @@ class GP_MarginalLogLikelihood(object):
         if s > 0:
             K += np.eye(x.size) * (s ** 2)
 
-        # invert K
+        # invert K and compute determinant
         L = np.linalg.cholesky(K)
         Li = inv(L)
         Ki = dot(Li.T, Li)
+        logdetK = 2 * log(diag(L)).sum()
 
+        # compute the marginal log likelihood
         t1 = -0.5 * dot(dot(y.T, Ki), y)
-        # The original computation here was -0.5 * log(det(K)), but
-        # this equals -0.5 * log(prod(diag(L))**2).
-        t2 = -log(diag(L)).sum()
+        t2 = -0.5 * logdetK
         t3 = -0.5 * x.size * log(2 * pi)
         mll = (t1 + t2 + t3)
         out = -mll
@@ -89,6 +157,32 @@ class GP_MarginalLogLikelihood(object):
         return out
 
     def jacobian(self, theta, x, y):
+        """Computes the negative Jacobian of the marginal log likelihood of
+        the kernel.
+
+        See Eq. 5.9 of Rasmussen & Williams (2006).
+
+        Parameters
+        ----------
+        theta : 3-tuple
+            The kernel parameters `h`, `w`, and `s`
+        x : numpy.ndarray with shape (n,)
+            The given input values
+        y : numpy.ndarray with shape (n,)
+            The given output values
+
+        Returns
+        -------
+        out : 3-tuple
+            The negative Jacobian of the marginal negative log
+            likelihood of the data given the parameters
+
+        References
+        ----------
+        Rasmussen, C. E., & Williams, C. K. I. (2006). Gaussian processes
+            for machine learning. MIT Press.
+
+        """
 
         h, w, s = theta
 
@@ -111,22 +205,48 @@ class GP_MarginalLogLikelihood(object):
         return -dmll
 
     def maximize(self, x, y, ntry=10, verbose=False):
+        """Find kernel parameter values which maximize the marginal log
+        likelihood of the data.
+
+        Parameters
+        ----------
+        x : numpy.ndarray with shape (n,)
+            The given input values
+        y : numpy.ndarray with shape (n,)
+            The given output values
+        ntry : integer (default=10)
+           Number of times to run MLII, to try to avoid local maxima
+        verbose : bool (default=False)
+           Print information about the optimization
+
+        Returns
+        -------
+        out : 3-tuple
+            The parameter values corresponding to the maximum marginal
+            log likelihood (that was found -- there might be a better
+            set of parameters, as this only finds local optima).
+
+        """
+
         args = np.empty((ntry, 3))
         fval = np.empty(ntry)
 
         for i in xrange(ntry):
+            # randomize starting parameter values
             h0 = np.random.uniform(0, np.ptp(y)**2)
             w0 = np.random.uniform(0, np.pi)
             s0 = np.random.uniform(0, np.sqrt(np.var(y)))
 
+            # run mimization function
             popt = opt.minimize(
                 fun=self,
                 x0=(h0, w0, s0),
                 args=(x, y),
                 jac=self.jacobian,
             )
-            success = popt['success']
 
+            # get results of the optimization
+            success = popt['success']
             if not success:
                 args[i] = np.nan
                 fval[i] = np.inf
@@ -138,6 +258,7 @@ class GP_MarginalLogLikelihood(object):
                 if verbose:
                     print "-MLL(%s) = %f" % (args[i], fval[i])
 
+        # choose the parameters that give the best MLL
         best = np.argmin(fval)
         if np.isinf(fval[best]) and sign(fval[best]) > 0:
             print args[best], fval[best]
@@ -146,8 +267,10 @@ class GP_MarginalLogLikelihood(object):
         return args[best]
 
 
-def fit_likelihood(ll, R, Sr, iix, cix):
-    """
+def similarity(I0, I1, sf=1):
+    """Computes the similarity between images `I0` and `I1`."""
+    diff = exp(np.sum(-0.5 * (I0 - I1)**2 / (sf**2))) + 1
+    return diff
 
     References
     ----------
