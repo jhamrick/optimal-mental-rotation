ec2552187e7449d5f80e1a72ee3518727506d716
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-06 13:11:38 -0700

    Fix model math

--- code/model.py
+++ code/model.py
@@ -3,18 +3,13 @@ import sympy as sym
 import scipy.optimize as opt
 
 from sympy.utilities.lambdify import lambdify
+from sympy.functions.special.delta_functions import DiracDelta as delta
 from numpy import pi
-from numpy import log, exp, sign, trace, dot, abs
-from numpy.linalg import inv, slogdet
+from numpy import log, exp, sign, trace, dot, abs, diag
+from numpy.linalg import inv
 
-import snippets.stats as stats
-
-reload(opt)
-reload(stats)
-GP = stats.GP
-make_kernel = stats.circular_gaussian_kernel
-# from snippets.stats import GP
-# from snippets.stats import circular_gaussian_kernel as make_kernel
+from snippets.stats import GP
+from snippets.stats import periodic_kernel as periodic_kernel
 
 
 def similarity(I0, I1, sf=1):
@@ -32,9 +27,9 @@ class GP_MarginalLogLikelihood(object):
         w = sym.Symbol('w')
         s = sym.Symbol('s')
 
-        k0 = h ** 2
-        k1 = sym.exp(-0.5 * (d ** 2) / (w ** 2))
-        self.sym_K = k0 * k1
+        k1 = (h ** 2) * sym.exp(-2. * sym.sin(d / 2.) ** 2 / (w ** 2))
+        k2 = (s ** 2) * delta(d)
+        self.sym_K = k1 + k2
 
         self.sym_dK_dh = sym.diff(self.sym_K, h)
         self.dK_dh = lambdify(((h, w, s), d), self.sym_dK_dh)
@@ -42,7 +37,8 @@ class GP_MarginalLogLikelihood(object):
         self.sym_dK_dw = sym.diff(self.sym_K, w)
         self.dK_dw = lambdify(((h, w, s), d), self.sym_dK_dw)
 
-        self.dK_ds = lambda theta, d: 2*theta[2] * (float(abs(d) < 1e-6))
+        self.sym_dK_ds = sym.diff(self.sym_K, s)
+        self.dK_ds = lambdify(((h, w, s), d), self.sym_dK_ds)
 
     @staticmethod
     def _d_dthetaj(Ki, dK_dthetaj, theta, x, y):
@@ -50,15 +46,12 @@ class GP_MarginalLogLikelihood(object):
         dj = np.empty((x.size, x.size))
         for i in xrange(x.size):
             for j in xrange(x.size):
-                d = x[i] - x[j]
-                if abs(d) > pi:
-                    diff = d - (sign(d) * 2 * pi)
-                else:
-                    diff = d
+                diff = x[i] - x[j]
                 dj[i, j] = dK_dthetaj((h, w, s), diff)
 
-        t0 = 0.5 * dot(y.T, dot(dot(Ki, dot(dj, Ki)), y))
-        t1 = -0.5 * trace(dot(Ki, dj))
+        k = dot(Ki, dj)
+        t0 = 0.5 * dot(y.T, dot(k, dot(Ki, y)))
+        t1 = -0.5 * trace(k)
         dd = t0 + t1
 
         return dd
@@ -77,11 +70,19 @@ class GP_MarginalLogLikelihood(object):
 
         # the overhead of JIT compiling isn't it worth it here because
         # this is just a temporary kernel function
-        K = make_kernel(h, w, s, jit=False)(x, x)
+        K = periodic_kernel(h, w, jit=False)(x, x)
+        if s > 0:
+            K += np.eye(x.size) * (s ** 2)
 
-        Ki = inv(K)
+        # invert K
+        L = np.linalg.cholesky(K)
+        Li = inv(L)
+        Ki = dot(Li.T, Li)
+
         t1 = -0.5 * dot(dot(y.T, Ki), y)
-        t2 = -0.5 * slogdet(K)[1]
+        # The original computation here was -0.5 * log(det(K)), but
+        # this equals -0.5 * log(prod(diag(L))**2).
+        t2 = -log(diag(L)).sum()
         t3 = -0.5 * x.size * log(2 * pi)
         mll = (t1 + t2 + t3)
         out = -mll
@@ -94,9 +95,14 @@ class GP_MarginalLogLikelihood(object):
 
         # the overhead of JIT compiling isn't it worth it here because
         # this is just a temporary kernel function
-        K = make_kernel(h, w, s, jit=False)(x, x)
+        K = periodic_kernel(h, w, jit=False)(x, x)
+        if s > 0:
+            K += np.eye(x.size) * (s ** 2)
 
-        Ki = inv(K)
+        # invert K
+        Li = inv(np.linalg.cholesky(K))
+        Ki = dot(Li.T, Li)
+
         dmll = np.array([
             self._d_dthetaj(Ki, self.dK_dh, theta, x, y),
             self._d_dthetaj(Ki, self.dK_dw, theta, x, y),
@@ -171,13 +177,13 @@ def fit_likelihood(ll, R, Sr, iix, cix):
     theta_S = ll.maximize(xi, yi)
     print theta_S
     print "Computing GP over S..."
-    mu_S, cov_S = GP(make_kernel(*theta_S), xi, yi, R)
+    mu_S, cov_S = GP(periodic_kernel(*theta_S), xi, yi, R)
     # GPR for log S
     print "Fitting parameters for GP over log S..."
     theta_logS = ll.maximize(xi, log(yi))
     print theta_logS
     print "Computing GP over log S..."
-    mu_logS, cov_logS = GP(make_kernel(*theta_logS), xi, log(yi), R)
+    mu_logS, cov_logS = GP(periodic_kernel(*theta_logS), xi, log(yi), R)
 
     # "candidate" points
     delta = mu_logS - log(mu_S)
@@ -189,7 +195,7 @@ def fit_likelihood(ll, R, Sr, iix, cix):
     theta_Dc = ll.maximize(xc, yc)
     print theta_Dc
     print "Computing GP over Dc..."
-    mu_Dc, cov_Dc = GP(make_kernel(*theta_Dc), xc, yc, R)
+    mu_Dc, cov_Dc = GP(periodic_kernel(*theta_Dc), xc, yc, R)
 
     return (delta, xi, yi, xc, yc,
             mu_S, cov_S, mu_logS, cov_logS, mu_Dc, cov_Dc)
