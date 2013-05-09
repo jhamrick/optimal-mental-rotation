92c2f99129c12c1936f9e2981e2564a01cea2672
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-09 12:57:35 -0700

    Generate figures from notebook

--- makefigs.sh
+++ makefigs.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -e
 
+# generate OmniGraffle figures
 for fig in figures/*.graffle; do
     echo "Exporting '$fig'..."
     omnigraffle-export -f png "$fig" figures/
 done
-
 /usr/bin/osascript -e 'tell application "OmniGraffle Professional 5" to quit'
+
+# now run analyses and generate figures from them
+cd "code"
+python ../run_nb.py notes.ipynb
