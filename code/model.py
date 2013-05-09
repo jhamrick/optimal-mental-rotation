e07000682766601ca8e013e347703419f8520530
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-09 12:20:45 -0700

    Add parametric Von Mises regression

--- code/model.py
+++ code/model.py
@@ -12,6 +12,14 @@ from snippets.stats import GP
 from snippets.stats import periodic_kernel
 from snippets.stats import gaussian_kernel
 
+import circstats as circ
+
+
+def similarity(I0, I1, sf=1):
+    """Computes the similarity between images `I0` and `I1`."""
+    S = exp(np.sum(-0.5 * ((I0 - I1) ** 2) / (sf ** 2)))
+    return S
+
 
 class KernelMLL(object):
     """Object representing the marginal log likelihood (MLL) of a
@@ -316,12 +324,6 @@ class KernelMLL(object):
         return args[best]
 
 
-def similarity(I0, I1, sf=1):
-    """Computes the similarity between images `I0` and `I1`."""
-    S = exp(np.sum(-0.5 * ((I0 - I1) ** 2) / (sf ** 2)))
-    return S
-
-
 class BayesianQuadrature(object):
     """Estimate a likelihood function, S(y|x) using Gaussian Process
     regressions, as in Osborne et al. (2012):
@@ -349,6 +351,8 @@ class BayesianQuadrature(object):
             Vector of x values
         y : numpy.ndarray
             Vector of (actual) y values
+        ntry : int (default=10)
+            Number of optimizations to run
         verbose : bool (default=False)
             Whether to print information during fitting
 
@@ -389,8 +393,6 @@ class BayesianQuadrature(object):
         ----------
         iix : numpy.ndarray
             Integer array of indices corresponding to the "given" x and y data
-        cix : numpy.ndarray
-            Integer array of indices corresponding to the "candidate" x points.
 
         References
         ----------
@@ -426,3 +428,155 @@ class BayesianQuadrature(object):
 
         # mean of the final regression for S
         self.mean = ((self.mu_S + 1) * (1 + self.delta)) - 1
+
+
+class VonMisesMSE(object):
+    """Object representing the mean squared error (MSE) of a
+    Von Mises PDF.
+
+    Methods
+    -------
+    __call__
+        Compute the MSE.
+    minimize
+        Find Von Mises PDF parameter values which minimize the MSE.
+
+    """
+
+    def __call__(self, theta, x, y):
+        """Computes the mean squared error of the Von Mises PDF.
+
+        Parameters
+        ----------
+        theta : 3-tuple
+            The Von Mises PDF parameters `thetahat`, `kappa`, and `z`
+        x : numpy.ndarray with shape (n,)
+            The given input values
+        y : numpy.ndarray with shape (n,)
+            The given output values
+
+        Returns
+        -------
+        out : float
+            The mean squared error of the PDF given the parameters
+
+        """
+
+        thetahat, kappa, z = theta
+        pdf = z * circ.vmpdf(x, thetahat, kappa)
+        err = np.sum((y - pdf) ** 2)
+        return err
+
+    def minimize(self, x, y, ntry=10, verbose=False):
+        """Find Von Mises parameter values which minimize the mean
+        squared error.
+
+        Parameters
+        ----------
+        x : numpy.ndarray with shape (n,)
+            The given input values
+        y : numpy.ndarray with shape (n,)
+            The given output values
+        ntry : integer (default=10)
+           Number of times to run optimization, to try to avoid local minima
+        verbose : bool (default=False)
+           Print information about the optimization
+
+        Returns
+        -------
+        out : 3-tuple
+            The parameter values corresponding to the minimum mean
+            squared error (that was found -- there might be a better set
+            of parameters, as this only finds local optima).
+
+        """
+
+        args = np.empty((ntry, 3))
+        fval = np.empty(ntry)
+
+        for i in xrange(ntry):
+            # randomize starting parameter values
+            t0 = np.random.uniform(0, 2*np.pi)
+            k0 = np.random.gamma(2, 2)
+            z0 = 1
+            p0 = (t0, k0, z0)
+
+            # run mimization function
+            popt = opt.minimize(
+                fun=self,
+                x0=p0,
+                args=(x, y),
+                method="L-BFGS-B",
+                bounds=((0, 2*np.pi), (0, None), (0, None))
+            )
+
+            # get results of the optimization
+            success = popt['success']
+            message = popt['message']
+
+            if not success:
+                args[i] = np.nan
+                fval[i] = np.inf
+                if verbose:
+                    print "Failed: %s" % message
+            else:
+                args[i] = abs(popt['x'])
+                fval[i] = popt['fun']
+                if verbose:
+                    print "MSE(%s) = %f" % (args[i], fval[i])
+
+        # choose the parameters that give the smallest MSE
+        best = np.argmin(fval)
+        if np.isinf(fval[best]) and sign(fval[best]) > 0:
+            print args[best], fval[best]
+            raise RuntimeError("Could not find parameter estimates")
+
+        return args[best]
+
+
+class ParametricRegression(object):
+
+    def __init__(self, x, y, mse, ntry=10, verbose=False):
+        """Initialize the likelihood estimator object.
+
+        Parameters
+        ----------
+        mll : VonMisesMSE object
+        x : numpy.ndarray
+            Vector of x values
+        y : numpy.ndarray
+            Vector of (actual) y values
+        ntry : int (default=10)
+            Number of optimizations to run
+        verbose : bool (default=False)
+            Whether to print information during fitting
+
+        """
+
+        # true x and y values for the likelihood
+        self.x = x.copy()
+        self.y = y.copy()
+        # mean squared error object
+        self.mse = mse
+        # number of optimization tries
+        self.ntry = ntry
+        # print fitting information
+        self.verbose = verbose
+
+    def fit(self, iix):
+        """Fit the likelihood function.
+
+        Parameters
+        ----------
+        iix : numpy.ndarray
+            Integer array of indices corresponding to the "given" x and y data
+
+        """
+
+        # input data
+        self.xi = self.x[iix].copy()
+        self.yi = self.y[iix].copy()
+
+        self.theta = self.mse.minimize(
+            self.xi, self.yi, ntry=self.ntry, verbose=self.verbose)
+        self.mean = self.theta[2] * circ.vmpdf(self.x, *self.theta[:2])
