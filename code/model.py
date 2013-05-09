47c7d3e7296c021d60f192e8bbb23034d2bee2d3
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-09 16:42:04 -0700

    Update similarity function

--- code/model.py
+++ code/model.py
@@ -17,7 +17,9 @@ import circstats as circ
 
 def similarity(I0, I1, sf=1):
     """Computes the similarity between images `I0` and `I1`."""
-    S = exp(np.sum(-0.5 * ((I0 - I1) ** 2) / (sf ** 2)))
+    Z = 1. / (np.sqrt(2 * np.pi) * sf)
+    e = exp(np.sum(-0.5 * ((I0 - I1) ** 2) / (sf ** 2)))
+    S = Z * e
     return S
 
 
