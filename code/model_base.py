d59ebd3d3797c667e51cb8eaa26173cffd161524
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-10 21:09:28 -0700

    Add Model base class

---
+++ code/model_base.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+
+class Model(object):
+
+    def __init__(self, R, S, dr, pR, **opt):
+        """Initialize the linear interpolation object.
+
+        Parameters
+        ----------
+        R : numpy.ndarray
+            Vector of all possible rotations, in radians. Each index i
+            should correspond to an angle i in degrees.
+        S : numpy.ndarray
+            Vector of all values of the similarity function S. Each
+            index i should correspond to an angle i in degrees.
+        dr : int
+            Angle of rotation in between sequential mental images
+        pR : numpy.ndarray
+            Probability vector for all values of R. Should have the same
+            dimensions as S.
+        **opt : Model options (see below)
+
+        Model options
+        -------------
+        verbose : bool (default=False)
+            Print information during the modeling process
+
+        """
+
+        # options
+        self.opt = opt.copy()
+
+        # step size
+        self.dr = dr
+
+        # prior over angles
+        self.pR = pR
+
+        # x and y values
+        self.R = R.copy()
+        self.S = S.copy()
+        self._rotations = np.arange(0, self.R.size, self.dr)
+
+        # samples
+        self.ix = [0, self.R.size-1]
+        # sampled R and S values
+        self.Ri = None
+        self.Si = None
+
+        # mean and variance of S
+        self.m_S = None
+        self.V_S = None
+
+        # mean and variance of Z
+        self.m_Z = None
+        self.V_Z = None
+
+    def sample(self, r):
+        S = self.S[r]
+        if r not in self.ix:
+            self.ix.append(r)
+            self.Ri = None
+            self.Si = None
+            if self.opt['verbose']:
+                print "R=% 3s degrees  S(X_b, X_R)=%f" % (r, S)
+        return S
+
+    def __iter__(self):
+        return self
+
+    def run(self):
+        for i in self:
+            if self.opt['verbose']:
+                self.fit()
+                self.integrate()
+                print "mu_Z  = %f" % self.m_Z
+                print "var_Z = %f" % self.V_Z
+
+        if self.Ri is None or self.Si is None:
+            self.fit()
+            self.integrate()
