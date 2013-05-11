40378ce52e502d58e84e61d41a751bf8b29b5905
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-05-10 21:09:44 -0700

    Rewrite NaiveModel to inherit from Model

--- code/model_naive.py
+++ code/model_naive.py
@@ -1,112 +1,59 @@
 import numpy as np
+from model_base import Model
 
 
-class NaiveModel(object):
+class NaiveModel(Model):
 
-    def __init__(self, x, y, step, verbose=False):
-        """Initialize the linear interpolation object.
+    def __init__(self, *args, **kwargs):
+        super(NaiveModel, self).__init__(*args, **kwargs)
+        self._icurr = 0
 
-        Parameters
-        ----------
-        x : numpy.ndarray
-            Vector of x values
-        y : numpy.ndarray
-            Vector of (actual) y values
-        step : int
-            Angle of rotation in between sequential mental images
-        verbose : bool (default=False)
-            Print information during the modeling process
+    def next(self):
+        """Sample the next point."""
 
-        """
+        inext = self._icurr + 1
+        iprev = self._icurr - 1
 
-        # true x and y values for the likelihood
-        self.x = x.copy()
-        self.y = y.copy()
-        # print fitting information
-        self.verbose = verbose
+        rcurr = self._rotations[self._icurr]
+        rnext = self._rotations[inext]
+        rprev = self._rotations[iprev]
 
-    def fit(self, iix):
-        """Fit the likelihood function.
+        scurr = self.sample(rcurr)
+        snext = self.sample(rnext)
+        sprev = self.sample(rprev)
 
-        Parameters
-        ----------
-        iix : numpy.ndarray
-            Integer array of indices corresponding to the "given" x and y data
+        if snext > scurr and snext > sprev:
+            self._icurr = inext
+        elif sprev > scurr and sprev > snext:
+            self._icurr = iprev
+        else:
+            raise StopIteration
 
-        """
+    def fit(self):
+        """Fit the likelihood function."""
 
-        # input data -- wrap around, so the interpolation works correctly
-        self.xi = np.concatenate([self.x[iix], self.x[iix][[0]] + 2*np.pi])
-        self.yi = np.concatenate([self.y[iix], self.y[iix][[0]]])
+        if self.opt['verbose']:
+           print "Fitting likelihood..."
 
-        self.mean = np.interp(self.x, self.xi, self.yi)
+        self.ix = sorted(self.ix)
+        self.Ri = self.R[self.ix]
+        self.Si = self.S[self.ix]
+        self.m_S = np.interp(self.R, self.Ri, self.Si)
+        self.V_S = np.zeros(self.m_S.shape)
 
-    def integrate(self, px):
-        """Compute the mean and variance of our estimate of the integral:
+    def integrate(self):
+        """Compute the mean and variance of Z:
 
-        $$Z = \int S(y|x)p(x) dx$$
-
-        Where S(y|x) is the function being estimated by `self.fit`.
-
-        Parameters
-        ----------
-        px : numpy.ndarray
-            Prior probabilities over x-values
-
-        Returns
-        -------
-        out : 2-tuple
-            The mean and variance of the integral
+        $$Z = \int S(X_b, X_R)p(R) dR$$
 
         """
 
-        m_Z = sum(px * self.mean)
-        V_Z = 0
-        return m_Z, V_Z
+        if self.m_S is None:
+            raise RuntimeError(
+                "self.m_S is not set, did you call self.fit first?")
 
-    def __iter__(self):
-        steps = np.arange(0, self.x.size, self.step)
-        iix = [0]
-        curr = 0
-        scurr = steps[curr]
+        if self.opt['verbose']:
+           print "Computing mean and variance of estimate of Z..."
 
-        if self.verbose:
-            print "Starting at R=%d" % scurr
-        yield scurr
-
-        while True:
-            next = curr + 1
-            prev = curr - 1
-            snext = steps[next]
-            sprev = steps[prev]
-
-            if next not in iix:
-                if self.verbose:
-                    print "R=% 3s degrees  S(X_b, X_R)=%f" % (
-                        snext, self.y[snext])
-                iix.append(next)
-                yield snext
-            if prev not in iix:
-                if self.verbose:
-                    print "R=% 3s degrees  S(X_b, X_R)=%f" % (
-                        sprev, self.y[sprev])
-                iix.append(prev)
-                yield sprev
-
-            if self.y[snext] > self.y[scurr]:
-                curr = next
-            elif self.y[sprev] > self.y[scurr]:
-                curr = prev
-            else:
-                break
-
-            scurr = steps[curr]
-            if self.verbose:
-                print "Pick R=%d" % scurr
-
-        if self.verbose:
-            print "Done"
-
-    def sample(self):
-        iix = sorted([i for i in self])
-        return iix
+        self.m_Z = sum(self.pR * self.m_S)
+        self.V_Z = 0
