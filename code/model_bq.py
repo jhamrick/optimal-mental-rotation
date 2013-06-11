54d7964a47751c0ea7ae77a0df99c7cfab5cd39c
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-06-10 22:20:09 -0700

    Handle nonnegativity constraint better

--- code/model_bq.py
+++ code/model_bq.py
@@ -4,7 +4,7 @@ from numpy import dot
 
 from model_base import Model
 
-from snippets.safemath import MIN_LOG
+from snippets.safemath import MIN_LOG, EPS
 from periodic_kernel import PeriodicKernel as kernel
 from gaussian_process import GP
 
@@ -64,8 +64,10 @@ class BayesianQuadratureModel(Model):
     def log_transform(self, x):
         return np.log((x / self.opt['gamma']) + 1)
 
-    def E(self, f, axis=-1):
-        pfx = f * self.opt['prior_R']
+    def E(self, f, axis=-1, p_R=None):
+        if p_R is None:
+            p_R = self.opt['prior_R']
+        pfx = f * p_R
         m = np.trapz(pfx, self.R, axis=axis)
         return m
 
@@ -195,24 +197,23 @@ class BayesianQuadratureModel(Model):
         self.debug("Fitting parameters for GP over %s ..." % name, level=2)
 
         # parameters / options
-        eps = np.finfo(float).eps
         ntry = self.opt['ntry_bq']
         verbose = self.opt['verbose'] > 3
 
         allkeys = ('h', 'w', 'p', 's')
         # minimization bounds
         allbounds = {
-            'h': (eps, None),
+            'h': (EPS, None),
             'w': (np.radians(self.opt['bq_wmin']),
                   np.radians(self.opt['bq_wmax'])),
-            'p': (eps, None),
+            'p': (EPS, None),
             's': (0, None)
         }
         # random initial value functions
         randf = {
-            'h': lambda: np.random.uniform(eps, np.max(np.abs(y))*2),
+            'h': lambda: np.random.uniform(EPS, np.max(np.abs(y))*2),
             'w': lambda: np.random.uniform(np.ptp(x) / 100., np.ptp(x) / 10.),
-            'p': lambda: np.random.uniform(eps, 2*np.pi),
+            'p': lambda: np.random.uniform(EPS, 2*np.pi),
             's': lambda: np.random.uniform(0, np.sqrt(np.var(y)))
         }
 
@@ -283,10 +284,6 @@ class BayesianQuadratureModel(Model):
         cix = sorted(ideal + self.ix)
         self.Rc = self.R[cix].copy()
         self.Dc = self.delta[cix].copy()
-        # use a crude thresholding here as our tilde transformation
-        # will fail if the mean goes below zero
-        eps = np.finfo(float).eps
-        self.Dc[self.Dc < eps] = eps
 
     def fit(self):
         """Run the GP regressions to fit the likelihood function.
@@ -311,48 +308,32 @@ class BayesianQuadratureModel(Model):
         self.gp_S = self._fit_gp(self.Ri, self.Si, "S")
         self.gp_logS = self._fit_gp(self.Ri, self.log_Si, "log(S)")
 
+        # use a crude thresholding here as our tilde transformation
+        # will fail if the mean goes below zero
+        self._S0 = np.clip(self.gp_S.m, EPS, np.inf)
+
         # fit delta, the difference between S and logS
-        self.delta = self.gp_logS.m - self.log_transform(self.gp_S.m)
+        self.delta = self.gp_logS.m - self.log_transform(self._S0)
         self.choose_candidates()
         self.gp_Dc = self._fit_gp(self.Rc, self.Dc, "Delta_c")
 
-        # the estimated mean of S
-        self.S_mean = self.gp_S.m + (
-            self.gp_S.m + self.opt['gamma'])*self.gp_Dc.m
-
+        self.S_mean = self._S0 + (self._S0 + self.opt['gamma'])*self.gp_Dc.m
         # the estimated variance of S
         dm_dw, Cw = self.dm_dw, self.Cw
-        self.S_cov = self.gp_logS.C + dot(dm_dw, dot(Cw, dm_dw))
+        self.S_cov = self.gp_logS.C + dot(dm_dw, dot(Cw, dm_dw.T))
+        self.S_cov[np.abs(self.S_cov) < np.sqrt(EPS)] = EPS
         self.S_var = np.diag(self.S_cov)
 
     ##################################################################
     # Mean
     @property
-    def _mean(self):
-        # Mean of int S(x) p(x) dx given Si
-        E_l = self.E(self.gp_S.m)
-        return E_l
-
-    @property
-    def _mean_correction(self):
-        # Mean of int delta(x) S(x) p(x) dx given Si and Dc
-        E_mdel_mS = self.E(self.gp_Dc.m * self.gp_S.m)
-        # Mean of int delta(x) p(x) dx given Si and Dc
-        E_mdel = self.E(self.gp_Dc.m)
-        # We need to multiply by gamma because we actually want to calculate
-        #   E[ (1 / gamma) (m_S + gamma) * m_del ]
-        # = (1 / gamma) (E[m_S * m_del] + gamma*E[m_del])
-        # We'll rescale by gamma later.
-        return E_mdel_mS + self.opt['gamma'] * E_mdel
-
-    @property
     def mean(self):
-        mean_ev = self._mean + self._mean_correction
+        mean_ev = self.E(self.S_mean)
         # sanity check
         if mean_ev < 0:
             print 'mean of evidence negative'
             print 'mean of evidence: %s' % mean_ev
-            mean_ev = self._mean
+            mean_ev = self._S0
         return mean_ev
 
     ##################################################################
@@ -367,7 +348,7 @@ class BayesianQuadratureModel(Model):
         inv_dKxx_dw = dot(inv_Kxx, dot(dKxx_dw, inv_Kxx))
         dm_dw = (dot(dKxox_dw, dot(inv_Kxx, self.log_Si)) -
                  dot(Kxox, dot(inv_dKxx_dw, self.log_Si)))
-        return dm_dw
+        return dm_dw[:, None]
 
     @property
     def Cw(self):
@@ -381,54 +362,26 @@ class BayesianQuadratureModel(Model):
         # scale.
         H_theta = self.gp_logS.d2lh_dtheta2()
         # XXX: fix this slicing
-        Cw = -1. / H_theta[1, 1]
+        Cw = np.array([[-1. / H_theta[1, 1]]])
         return Cw
 
     @property
-    def _var(self):
-        x, xs, p_R = self.R, self.Ri, self.opt['prior_R']
-        gamma = self.opt['gamma']
-        C_lS = self.gp_logS.C
-        m_S = self.gp_S.m
-
-        # int int C_(lS|s) p(x) p(x') dx dx'
-        E_ClS = self.E(self.E(C_lS))
-
-        # int int p(x) p(x') C_(lS|s)(x, x') m_(l|s)(x') dx dx'
-        E_ClS_mS = self.E(self.E(C_lS * m_S[None, :]))
-
-        # int dx p(x) int dx' p(x') m_(l|s)(x) C_(lS|s)(x, x') m_(l|s)(x')
-        E_mS_ClS_mS = self.E(self.E(C_lS * m_S[None, :] * m_S[:, None]))
-
-        # Again, we need to mulSiply by gamma because:
-        #   E[E[(1/gamma**2) (m_S + gamma) * C_lS * (m_S + gamma)]]
-        # = (1 / gamma**2) * (
-        #      gamma**2 * E[E[C_lS]] +
-        #      2*gamma * E[E[m_S * C_lS]] +
-        #      E[E[m_S * C_lS * m_S]])
-        var_ev = (gamma**2 * E_ClS) + (2*gamma * E_ClS_mS) + E_mS_ClS_mS
-        return var_ev
-
-    @property
-    def _var_correction(self):
-        dm_dw, Cw = self.dm_dw, self.Cw
-        m_S = self.gp_S.m
-
-        E_mS_dmlSdw = self.E(m_S * dm_dw)
-        E_dmlSdw = self.E(dm_dw)
-
-        correction = Cw * (E_mS_dmlSdw + self.opt['gamma']*E_dmlSdw) ** 2
-        return correction
-
-    @property
     def var(self):
-        var_ev = self._var + self._var_correction
+        m_S = self.gp_S.m + self.opt['gamma']
+        mCm = self.S_cov * m_S[:, None] * m_S[None, :]
+        var_ev = self.E(self.E(mCm))
         # sanity check
         if var_ev < 0:
-            print 'variance of evidence negative'
             print 'variance of evidence: %s' % var_ev
-            var_ev = np.exp(MIN_LOG)
-        return var_ev
+            raise RuntimeError('variance of evidence negative')
+
+        # compute coarse lower bound for variance, because we know it
+        # can't go below zero
+        lower = self.S_mean - np.sqrt(self.S_var)
+        p_R = (lower >= EPS).astype('f8')
+        p_R = p_R / (np.sum(p_R) * (self.R[1] - self.R[0]))
+        var_ev_low = self.E(self.E(mCm, p_R=p_R), p_R=p_R)
+        return var_ev_low, var_ev
 
     def integrate(self):
         """Compute the mean and variance of Z:
@@ -448,8 +401,8 @@ class BayesianQuadratureModel(Model):
             raise RuntimeError(
                 "S_mean or S_var is not set, did you call self.fit first?")
 
-        self.Z_mean, var = self.mean, self.var
-        self.Z_var = (var, var)
+        self.Z_mean = self.mean
+        self.Z_var = self.var
         self.print_Z(level=0)
 
 
