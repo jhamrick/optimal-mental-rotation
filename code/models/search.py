83d20e5e59dbe5baab522bfe69ff9ffd567e6305
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-07-04 17:54:42 -0700

    Oops, fix bug in hill climbing search so we actually stop

--- code/models/search.py
+++ code/models/search.py
@@ -8,20 +8,29 @@ def hill_climbing(model):
         return False
 
     rcurr, scurr = model.curr_val
-    model.debug("Current value: %f" % scurr, level=2)
+    d = int(np.round(np.degrees(rcurr)))
+    model.debug("Current: S(%d) = %f" % (d, scurr), level=1)
 
     # this gets the next rotation and similarity, continuing in the
     # direction that we had previously been rotating
     rnext, snext = model.next_val()
-    model.debug("Rotate to %d degrees" % np.degrees(rnext), level=2)
 
-    thresh = model.opt.get('h', np.sqrt(model.opt['scale'] / 100.)) ** 2
+    thresh = model.opt.get('h', None)
+    if thresh is None:
+        thresh = np.sqrt(model.opt['scale'] / 100.)
+    thresh = thresh ** 2
 
-    # if the next value is less than the current value, try turning
-    # around
     if scurr > thresh and snext < scurr:
+        # if the next value is less than the current value, try
+        # turning around -- first go back to the current sample
         model.sample(rcurr)
-        model.debug("Rotate to %d degrees" % np.degrees(rcurr), level=2)
+        # then go one further
+        rprev, sprev = model.next_val()
+        # if this sample is still less than current, then we've found
+        # a maximum
+        if sprev < scurr:
+            return False
+
     elif scurr <= thresh:
         model.debug("Below threshold, ignoring maxima/minima!", level=2)
 
