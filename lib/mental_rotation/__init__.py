b87ff5a28ea7dd81d3ac50b96b222d394b111717
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-12-02 14:37:51 -0800

    Create mental rotation module

---
+++ lib/mental_rotation/__init__.py
@@ -0,0 +1,31 @@
+from path import path
+ROOT_PATH = path(__path__[0]).joinpath("../../").abspath()
+del path
+
+
+def get_config(name='config.ini'):
+    from ConfigParser import SafeConfigParser
+    config = SafeConfigParser()
+    config.read(ROOT_PATH.joinpath(name))
+    return config
+
+config = get_config()
+
+
+def get_path(name):
+    return ROOT_PATH.joinpath(config.get("paths", name))
+
+STIM_PATH = get_path("stimuli")
+DATA_PATH = get_path("data")
+FIG_PATH = get_path("figures")
+BIN_PATH = get_path("bin")
+EXP_PATH = get_path("experiment")
+
+import logging
+LOGLEVEL = config.get("global", "loglevel").upper()
+FORMAT = '%(levelname)s -- %(processName)s/%(filename)s -- %(message)s'
+logging.basicConfig(level=LOGLEVEL, format=FORMAT)
+
+import numpy as np
+DTYPE = np.dtype(config.get("global", "dtype"))
+del np
