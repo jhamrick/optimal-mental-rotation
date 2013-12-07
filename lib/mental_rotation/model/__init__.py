d9e31be948ae7af1de34d811c91064aa310dc021
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-12-07 15:38:39 -0800

    Include BQ model in model.__init__.py

--- lib/mental_rotation/model/__init__.py
+++ lib/mental_rotation/model/__init__.py
@@ -1,5 +1,7 @@
 from base import BaseModel
 from gold_standard import GoldStandardModel
 from hill_climbing import HillClimbingModel
+from bayesian_quadrature import BayesianQuadratureModel
 
-__all__ = ['BaseModel', 'GoldStandardModel', 'HillClimbingModel']
+__all__ = ['BaseModel', 'GoldStandardModel', 'HillClimbingModel',
+           'BayesianQuadratureModel']
