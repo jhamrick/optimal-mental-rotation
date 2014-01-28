ab14720af9a121545a243ee9714181cd6e2b1c06
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-01-28 14:17:51 -0800

    Revert "Update threshold model, so prior has an effect on threhsold"
    This reverts commit bc546b62ba63a25ca4d479bcd84bbe39f55df605.

--- lib/mental_rotation/model/threshold_model.py
+++ lib/mental_rotation/model/threshold_model.py
@@ -10,7 +10,7 @@ class ThresholdModel(BaseModel):
     def __init__(self, *args, **kwargs):
         super(ThresholdModel, self).__init__(*args, **kwargs)
         self.direction = None
-        self._thresh = 0.8 * 0.5 * np.exp(self._log_const)
+        self._thresh = 0.8 * np.exp(self._log_const)
 
     ##################################################################
     # Sampling
@@ -31,7 +31,7 @@ class ThresholdModel(BaseModel):
                 self._current_iter += 1
                 self.model['F'].value = 0
 
-            if self.hypothesis_test() is not None:
+            if np.exp(self.model['log_S'].logp) > self._thresh:
                 self.status = 'done'
 
             return
@@ -42,15 +42,17 @@ class ThresholdModel(BaseModel):
             self.direction = np.sign(step)
             self.model['R'].value = step
 
-            if self.hypothesis_test() is not None:
+            new_log_S = self.model['log_S'].logp
+            if np.exp(new_log_S) > self._thresh:
                 self.status = 'done'
                 return
 
         else:
             step = self.direction * np.abs(self._random_step())
             self.model['R'].value = R + step
+            new_log_S = self.model['log_S'].logp
 
-            if self.hypothesis_test() is not None:
+            if np.exp(new_log_S) > self._thresh:
                 self.status = 'done'
                 return
 
@@ -63,37 +65,6 @@ class ThresholdModel(BaseModel):
 
             else: # pragma: no cover
                 pass
-    ##################################################################
-    # Log likelihoods for each hypothesis
-
-    @property
-    def log_lh_h0(self):
-        Fi = self.F_i == 0
-        if Fi.any():
-            log_S0 = self.log_S_i[Fi].max()
-        else:
-            log_S0 = -np.inf
-        return log_S0
-
-    @property
-    def log_lh_h1(self):
-        Fi = self.F_i == 1
-        if Fi.any():
-            log_S1 = self.log_S_i[Fi].max()
-        else:
-            log_S1 = -np.inf
-        return log_S1
-
-    def hypothesis_test(self):
-        llh0 = self.log_lh_h0 + np.log(self.opts['prior'])
-        llh1 = self.log_lh_h1 + np.log(1 - self.opts['prior'])
-
-        if llh0 > llh1 and llh0 > np.log(self._thresh):
-            return 0
-        elif llh1 > llh0 and llh1 > np.log(self._thresh):
-            return 1
-        else:
-            return None
 
     ##################################################################
     # Copying/Saving
