6e00d4c316847fed34f1e49b1e54f1bd2ca4f6aa
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-04-27 16:53:44 -0700

    Break worker_job into separate functions

--- lib/mental_rotation/sims/client.py
+++ lib/mental_rotation/sims/client.py
@@ -70,6 +70,43 @@ def do(func, *args):
     return output
 
 
+def handle_task(task_json, tmpdir):
+    task = json.loads(task_json)
+    task_name = task["task_name"]
+    task["data_path"] = tmpdir.joinpath("%s.h5" % task_name)
+
+    logger.info("Got task '%s'", task_name)
+    startime = datetime.now()
+    error = simulate(task)
+    dt = (datetime.now() - startime).total_seconds()
+    logger.info("Completed task '%s' in %s seconds", task_name, dt)
+
+    return task_name, task['data_path'], error
+
+
+def send_data(task_name, data_path, host, port, sim_root):
+    # then send it to the server
+    if host in ('localhost', '127.0.0.1'):
+        prefix = ""
+        dst_path = "%s/%s.h5" % (sim_root, task_name)
+        options = []
+    else:
+        prefix = "ubuntu@%s:" % host
+        dst_path = sim_root.joinpath("%s.h5" % task_name)
+        options = [
+            '-o', 'StrictHostKeyChecking=no',
+            '-o', 'UserKnownHostsFile=/dev/null'
+        ]
+
+    # build the scp command
+    cmd = ['scp', '-q']
+    cmd.extend(options)
+    cmd.append(data_path)
+    cmd.append(prefix + dst_path)
+
+    do(run_command, logger, cmd)
+
+
 def worker_job(host, port):
     # connet to the server
     pandaserver = ServerProxy("http://%s:%d" % (host, port))
@@ -85,49 +122,25 @@ def worker_job(host, port):
             logger.info("Nothing left to do, shutting down")
             break
 
-        task = json.loads(task)
-        task_name = task["task_name"]
-        task["data_path"] = tmpdir.joinpath("%s.h5" % task_name)
-
-        logger.info("Got task '%s'", task_name)
-        startime = datetime.now()
-        error = simulate(task)
-        dt = (datetime.now() - startime).total_seconds()
-        logger.info("Completed task '%s' in %s seconds", task_name, dt)
+        # process the task that we got
+        task_name, data_path, error = handle_task(task)
 
         if error is not None:
+            # report errors, if they occurred
             logger.error("Task '%s' failed with error:\n%s", task_name, error)
             do(pandaserver.panda_error, task_name)
 
         else:
-            data_path = path(task["data_path"])
-
-            # then send it to the server
-            if host in ('localhost', '127.0.0.1'):
-                prefix = ""
-                dst_path = "%s/%s.h5" % (sim_root, task_name)
-                options = []
-            else:
-                prefix = "ubuntu@%s:" % host
-                dst_path = sim_root.joinpath("%s.h5" % task_name)
-                options = [
-                    '-o', 'StrictHostKeyChecking=no',
-                    '-o', 'UserKnownHostsFile=/dev/null'
-                ]
-
-            # build the scp command
-            cmd = ['scp', '-q']
-            cmd.extend(options)
-            cmd.append(data_path)
-            cmd.append(prefix + dst_path)
-
-            do(run_command, logger, cmd)
+            # scp the data to the server
+            send_data(task_name, data_path, host, port, sim_root)
 
             # then mark it as complete
             do(pandaserver.panda_complete, task_name)
 
-        task['data_path'].rmtree_p()
+        # clean up temporary data
+        data_path.remove()
 
+    # clean up temporary directory
     tmpdir.rmtree_p()
 
 
