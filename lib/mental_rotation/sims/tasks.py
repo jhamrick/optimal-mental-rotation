ab01287db430e5428c90ef7c7cad71858bc25cd4
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-06-05 10:35:38 -0700

    Clean up tasks.py

--- lib/mental_rotation/sims/tasks.py
+++ lib/mental_rotation/sims/tasks.py
@@ -17,6 +17,36 @@ class Tasks(dict):
             tasks.update(json.load(fh))
         return tasks
 
+    @staticmethod
+    def _make_chunks(model_opts, num_samples, chunksize):
+        # make the list of parameter names that we want for each simulation
+        opt_names = sorted(model_opts.keys())
+        param_names = opt_names + ['sample']
+
+        # make a list of lists, where each inner list contains the
+        # possible parameter values for one particular parameter
+        param_vals = [model_opts[key] for key in param_names[:-1]]
+        param_vals.append(range(num_samples))
+
+        # make a list of all parameter combinations
+        combs = np.array(list(product(*param_vals)))
+
+        # compute the number of chunks
+        if chunksize == -1:
+            n_chunks = 1
+        else:
+            n_chunks = int(np.ceil(combs.shape[0] / chunksize))
+
+        ichunks = np.array_split(
+            np.arange(len(combs)), n_chunks, axis=0)
+
+        chunks = []
+        for idx, ichunk in enumerate(ichunks):
+            chunk = {sim: dict(zip(param_names, combs[sim])) for sim in ichunk}
+            chunks.append(chunk)
+
+        return chunks
+
     @classmethod
     def create(cls, params):
         """Create the tasks dictionary from the parameters."""
@@ -36,41 +66,25 @@ class Tasks(dict):
             stim = path(stim)
             rot = float(stim.namebase.split("_")[1])
             if rot in (0, 180):
-                ns = num_samples * 2
+                chunks = cls._make_chunks(opts, num_samples * 2, chunksize)
             else:
-                ns = num_samples
+                chunks = cls._make_chunks(opts, num_samples, chunksize)
 
-            opt_names = sorted(opts.keys())
-            param_names = opt_names + ['sample']
-            param_vals = [opts[key] for key in param_names[:-1]]
-            param_vals.append(range(ns))
-
-            combs = np.array(list(product(*param_vals)))
-            if chunksize == -1:
-                n_chunks = 1
-            else:
-                n_chunks = int(np.ceil(combs.shape[0] / chunksize))
-
-            ichunks = np.array_split(
-                np.arange(len(combs)), n_chunks, axis=0)
-
-            for idx, ichunk in enumerate(ichunks):
-                sim_name = "%s~%d" % (stim.namebase, idx)
-                data_path = sim_root.joinpath(sim_name)
-                model_opts = {
-                    i: dict(zip(param_names, combs[i])) for i in ichunk}
+            for idx, chunk in enumerate(chunks):
+                task_name = "%s~%d" % (stim.namebase, idx)
+                data_path = sim_root.joinpath(task_name)
 
                 # Make the task dicts for this sample.
-                tasks[sim_name] = {
+                tasks[task_name] = {
                     "model": params["model"],
                     "istim": istim,
                     "stim_path": str(stim),
                     "data_path": str(data_path),
-                    "task_name": sim_name,
-                    "seed": abs(hash(sim_name)),
-                    "model_opts": model_opts,
+                    "task_name": task_name,
+                    "seed": abs(hash(task_name)),
+                    "model_opts": chunk,
                 }
 
-                completed[sim_name] = False
+                completed[task_name] = False
 
         return tasks, completed
